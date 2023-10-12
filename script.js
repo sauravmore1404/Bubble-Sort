29e71343d7b6c5318831272eccde4446ba18aab9
Saurav More <98641972+sauravmore1404@users.noreply.github.com>
2023-10-12 12:25:21 +0530

    Create script file

---
+++ script.js
@@ -0,0 +1,10 @@
+const sort = (array) => {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return (document.getElementById('demo').innerHTML = array);
+};
